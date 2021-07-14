c00ae977d38189ef34a9414df4fe8d65173ca516
Dimas Nur Alif Rochmadtulloh <dimasnur.dev@gmail.com>
2021-07-14 18:45:12 +0700

    create app theme

---
+++ lib/config/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppColor {
+  static const Color background = Color(0xFFF0F0E4);
+  static const Color secondaryBackground = Color(0xFFE7E4D3);
+  static const Color green = Color(0xFF2AAF61);
+}
+
+class AppTheme {
+  static ThemeData of(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return theme.copyWith(
+      appBarTheme: const AppBarTheme(backgroundColor: AppColor.background),
+      scaffoldBackgroundColor: AppColor.background,
+      primaryColor: Colors.white,
+      accentColor: AppColor.green,
+      iconTheme: const IconThemeData(color: Colors.black),
+      textTheme: GoogleFonts.montserratTextTheme(),
+      primaryTextTheme: GoogleFonts.montserratTextTheme(),
+      accentTextTheme: GoogleFonts.montserratTextTheme(),
+    );
+  }
+}
