a3bb1e5464e1f48edafc8a8065c1cd9b1cfa7a51
Dimas Nur Alif Rochmadtulloh <dimasnur.dev@gmail.com>
2021-07-15 17:09:41 +0700

    create base room page

---
+++ lib/presetation/pages/room_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_clubhouse/data/entities/room_entity.dart';
+
+class RoomPage extends StatelessWidget {
+  final RoomEntity room;
+
+  const RoomPage({
+    Key? key,
+    required this.room,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/presetation/widgets/room_card.dart
+++ lib/presetation/widgets/room_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_clubhouse/data/data_providers/dummy_data.dart';
-import 'package:flutter_clubhouse/data/entities/room_entity.dart';
-import 'package:flutter_clubhouse/presetation/widgets/widget.dart';
+import 'package:flutter_clubhouse/presetation/pages/room_page.dart';
+
+import '../../data/entities/entity.dart';
+import 'widget.dart';
 
 class RoomCard extends StatelessWidget {
   final RoomEntity roomEntity;
@@ -14,85 +15,97 @@ class RoomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${roomEntity.club} 🏠'.toUpperCase(),
-                style: Theme.of(context).textTheme.overline?.copyWith(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 1,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (_) => RoomPage(room: roomEntity),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${roomEntity.club} 🏠'.toUpperCase(),
+                  style: Theme.of(context).textTheme.overline?.copyWith(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1,
+                      ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  roomEntity.name,
+                  style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: StackImageRoomCard(roomEntity: roomEntity),
                     ),
-                overflow: TextOverflow.ellipsis,
-              ),
-              Text(
-                roomEntity.name,
-                style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              const SizedBox(height: 12),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: StackImageRoomCard(roomEntity: roomEntity),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ...roomEntity.speakers.map(
-                          (speaker) => Text(
-                            '${speaker.givenName} ${speaker.familyName} 💬',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText1
-                                ?.copyWith(fontSize: 16),
-                            overflow: TextOverflow.ellipsis,
+                    Expanded(
+                      flex: 2,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ...roomEntity.speakers.map(
+                            (speaker) => Text(
+                              '${speaker.givenName} ${speaker.familyName} 💬',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1
+                                  ?.copyWith(fontSize: 16),
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 4,
-                        ),
-                        Text.rich(
-                          TextSpan(children: [
-                            TextSpan(
-                                text:
-                                    '${roomEntity.speakers.length + roomEntity.followedBySpeakers.length + roomEntity.others.length} '),
-                            const WidgetSpan(
-                              child: Icon(
-                                CupertinoIcons.person_fill,
-                                size: 18,
-                                color: Colors.grey,
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Text.rich(
+                            TextSpan(children: [
+                              TextSpan(
+                                  text:
+                                      '${roomEntity.speakers.length + roomEntity.followedBySpeakers.length + roomEntity.others.length} '),
+                              const WidgetSpan(
+                                child: Icon(
+                                  CupertinoIcons.person_fill,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
                               ),
-                            ),
-                            TextSpan(text: ' / ${roomEntity.speakers.length} '),
-                            const WidgetSpan(
-                              child: Icon(
-                                CupertinoIcons.chat_bubble_fill,
-                                size: 18,
-                                color: Colors.grey,
+                              TextSpan(
+                                  text: ' / ${roomEntity.speakers.length} '),
+                              const WidgetSpan(
+                                child: Icon(
+                                  CupertinoIcons.chat_bubble_fill,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
                               ),
-                            ),
-                          ]),
-                          style: TextStyle(color: Colors.grey[600]),
-                        ),
-                      ],
+                            ]),
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
